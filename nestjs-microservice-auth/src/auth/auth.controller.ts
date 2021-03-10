ee4221811ef6989b513352693917fc46590c0d32
Evgeny Arol <arolevgeny@gmail.com>
2021-03-10 20:23:59 +0300

    add update method + jwt verification

--- nestjs-microservice-auth/src/auth/auth.controller.ts
+++ nestjs-microservice-auth/src/auth/auth.controller.ts
@@ -1,34 +1,71 @@
-import { Controller, Post, Body, Get, Param, UseGuards, Request, Logger } from '@nestjs/common';
+import { Controller, Post, Body, Put, Param, UseGuards, Request, Logger, Response, Get } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
 import { MessagePattern } from '@nestjs/microservices';
 import { User } from './auth.entity';
 import { DoesUserExist } from './guards/DoesUserExist';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { upload } from './uploadimage'
 
 @Controller()
 export class AuthController {
   constructor(
     private readonly authService: AuthService) { }
 
-  @UseGuards(LocalAuthGuard)
-  @Post('signin')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
+  // Sign In
   @UseGuards(DoesUserExist)
   @Post('signup')
-  async signUp(@Body() user : User) {
-    return this.authService.createUser(user);
-
+  async signUp(@Body() user: User) {
+    return this.authService.register(user);
   }
 
   @UseGuards(DoesUserExist)
   @Post('signup/:refUrl')
-  async signUpWithRef(@Request() req, @Body() user : User){
-      const refUrl = req.params.refUrl
-      return this.authService.createUserWithRef(user, refUrl);
+  async signUpWithRef(@Request() req, @Body() user: User) {
+    const refUrl = req.params.refUrl
+    return this.authService.registerWithRef(user, refUrl);
   }
+
+  // Sign Up
+  @UseGuards(LocalAuthGuard)
+  @Post('signin')
+  async signIn(@Request() request) {
+    return this.authService.login(request.user);
+  }
+
+  // Update avatar
+  @Put('profile/avatar/:id')
+  @UseGuards(JwtAuthGuard)
+  async updateProfileAvatar(@Param('id') id: number, @Body() user: User, @Response() response, @Request() request) {
+    const singleUpload = upload.single("avatar");
+    await singleUpload(request, response, (err) => {
+      if (err) {
+        return response.status(422).send({
+          errors: [{ title: "File Upload Error", detail: err.message }]
+        });
+      }
+      user.avatar = request.file.location
+      this.authService.updateUserById(id, user)
+      return response.json(user);
+    });
+  }
+
+  // Update profile
+  @Put('profile/:id')
+  @UseGuards(JwtAuthGuard)
+  async updateProfile(@Param('id') id: number, @Body() user: User, @Response() response) {
+    this.authService.updateUserById(id, user)
+    return response.json(user);
+  }
+
+
+  // Test
+  @Get('whoami')
+  @UseGuards(JwtAuthGuard)
+  public async testAuth(@Request() request: any): Promise<any> {
+    return request.user;
+  }
+
 
   @MessagePattern({ role: 'auth', cmd: 'check' })
   async loggedIn(data) {
