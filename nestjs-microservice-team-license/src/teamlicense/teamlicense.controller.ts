b253ec0405f9416ff0bc2846beab31a236bc77d6
Evgeny Arol <arolevgeny@gmail.com>
2021-03-13 20:51:04 +0300

    add check for jwt

--- nestjs-microservice-team-license/src/teamlicense/teamlicense.controller.ts
+++ nestjs-microservice-team-license/src/teamlicense/teamlicense.controller.ts
@@ -2,7 +2,7 @@ import { Controller, UseGuards, Get, Post, Body, Request, Response, Put, Param, 
 import { MessagePattern } from '@nestjs/microservices';
 import { TeamLicense } from './teamlicense.entity';
 import { TeamLicenseService } from './teamlicense.service';
-
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 @Controller()
 export class TeamLicenseController {
@@ -12,17 +12,20 @@ export class TeamLicenseController {
 
   // TeamLicense
   @Get('teamlicense/:id')
+  @UseGuards(JwtAuthGuard)
   async getTeamLicense(@Request() request) {
     const id = request.params.id
     return await this.teamLicenseService.getTeamLicenseById(id)
   }
-
+  
   @Post('teamlicense')
+  @UseGuards(JwtAuthGuard)
   async saveTeamLicense(@Body() teamLicense: TeamLicense) {
     return this.teamLicenseService.saveTeamLicense(teamLicense);
   }
-
+  
   @Put('teamlicense/:id')
+  @UseGuards(JwtAuthGuard)
   async updateTeamLicense(@Param('id') id: number, @Body() teamLicense: TeamLicense, @Response() response) {
     this.teamLicenseService.updateTeamLicenseById(id, teamLicense);
     response.json(teamLicense)
--- nestjs-microservice-team/src/team/team.controller.ts
+++ nestjs-microservice-team/src/team/team.controller.ts
@@ -2,7 +2,7 @@ import { Controller, UseGuards, Get, Post, Body, Request, Response, Put, Param, 
 import { MessagePattern } from '@nestjs/microservices';
 import { Team } from './team.entity';
 import { TeamService } from './team.service';
-
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 @Controller()
 export class TeamController {
@@ -12,17 +12,20 @@ export class TeamController {
 
   // Team
   @Get('team/:id')
+  @UseGuards(JwtAuthGuard)
   async getTeamLicense(@Request() request) {
     const id = request.params.id
     return await this.teamService.getTeamById(id)
   }
 
   @Post('team')
+  @UseGuards(JwtAuthGuard)
   async saveBrowserProfile(@Body() team: Team) {
     return this.teamService.saveTeam(team);
   }
 
   @Put('team/:id')
+  @UseGuards(JwtAuthGuard)
   async updateTeam(@Param('id') id: number, @Body() team: Team, @Response() response) {
     this.teamService.updateTeamById(id, team);
     response.json(team)
