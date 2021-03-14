08384ff89368a4599d8e10dfcda6d824c3a164d2
Evgeny Arol <arolevgeny@gmail.com>
2021-03-14 20:59:25 +0300

    add communication for microsevres

--- nestjs-microservice-team-license/src/teamlicense/teamlicense.service.ts
+++ nestjs-microservice-team-license/src/teamlicense/teamlicense.service.ts
@@ -1,29 +1,74 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, Inject, RequestTimeoutException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { TeamLicense } from './teamlicense.entity';
+import { TeamLicenseDto } from './../dto/teamlicense.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { timeout, catchError } from 'rxjs/operators';
+import { TimeoutError, throwError } from 'rxjs';
 
 @Injectable()
 export class TeamLicenseService {
     constructor(
+        @Inject('AUTH_CLIENT')
+        private readonly client: ClientProxy,
         @InjectRepository(TeamLicense)
         private readonly teamLicenseRepository: Repository<TeamLicense>
     ) { }
 
-    async saveTeamLicense(teamLicense: TeamLicense): Promise<TeamLicense> {
-        return await this.teamLicenseRepository.save({ ...teamLicense });
+    async saveTeamLicense(teamLicense: TeamLicense): Promise<TeamLicenseDto> {
+        const result = await this.teamLicenseRepository.save({ ...teamLicense });
+        try {
+            const creatorId = await this.client.send({ role: 'user', cmd: 'get' }, +result.creatorId)
+                .pipe(
+                    timeout(5000),
+                    catchError(err => {
+                        if (err instanceof TimeoutError) {
+                            return throwError(new RequestTimeoutException());
+                        }
+                        return throwError(err);
+                    }))
+                .toPromise();
+            const data = {
+                id: result.id,
+                maxProfiles: result.maxProfiles,
+                dieTo: result.dieTo,
+                maxSubAccounts: result.maxSubAccounts,
+                creatorId: creatorId
+            }
+            return data
+        }
+        catch (e) {
+            Logger.log(e);
+            throw e;
+        }
+
+
     }
 
     async getTeamLicenseById(id: number): Promise<TeamLicense> {
-        const teamLicense = await this.teamLicenseRepository.findOne({ where: { id } }); 
-        if (!teamLicense) {
-            return null
+        try {
+            const result = await this.teamLicenseRepository.findOne({ where: { id } })
+            result.creatorId = await this.client.send({ role: 'user', cmd: 'get' }, +result.creatorId)
+                .pipe(
+                    timeout(5000),
+                    catchError(err => {
+                        if (err instanceof TimeoutError) {
+                            return throwError(new RequestTimeoutException());
+                        }
+                        return throwError(err);
+                    }))
+                .toPromise();
+            return result
         }
-        return teamLicense
+        catch (e) {
+            Logger.log(e);
+            throw e;
+        }
     }
 
-    async updateTeamLicenseById(id : number, teamLicense: TeamLicense): Promise<any> {
+    async updateTeamLicenseById(id: number, teamLicense: TeamLicense): Promise<any> {
         return await this.teamLicenseRepository.update(id, teamLicense);
     }
-    
+
 }
